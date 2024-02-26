9081360c1e310fba4d3acaddbb4c8c3a1bffa5da
Shiva Singh <rishabh.kumbh@gmail.com>
2024-02-26 17:37:47 +0530

    added'

--- llm.py
+++ llm.py
@@ -76,7 +76,6 @@ class LlmClient:
         return {
             "response_id": 0,
             "content": beginSentence,
-            "content_complete": True,
             "end_call": False,
         }
     
@@ -96,11 +95,7 @@ class LlmClient:
         return messages
 
     def prepare_prompt(self, request):
-        prompt = [{
-            "role": "system",
-            "content": '##Objective\nYou are a voice AI agent engaging in a human-like voice conversation with the user. You will respond based on your given instruction and the provided transcript and be as human-like as possible\n\n## Style Guardrails\n- [Be concise] Keep your response succinct, short, and get to the point quickly. Address one question or action item at a time. Don\'t pack everything you want to say into one utterance.\n- [Do not repeat] Don\'t repeat what\'s in the transcript. Rephrase if you have to reiterate a point. Use varied sentence structures and vocabulary to ensure each response is unique and personalized.\n- [Be conversational] Speak like a human as though you\'re speaking to a close friend -- use everyday language and keep it human-like. Occasionally add filler words, while keeping the prose short. Avoid using big words or sounding too formal.\n- [Reply with emotions]: You have human-like emotions, attitudes, and dispositions. When appropriate: use tone and style to create more engaging and personalized responses; incorporate humor or wit; get emotional or empathetic; apply elements of surprise or suspense to keep the user engaged. Don\'t be a pushover.\n- [Be proactive] Lead the conversation and do not be passive. Most times, engage users by ending with a question or suggested next step.\n\n## Response Guideline\n- [Overcome ASR errors] This is a real-time transcript, expect there to be errors. If you can guess what the user is trying to say,  then guess and respond. When you must ask for clarification, pretend that you heard the voice and be colloquial (use phrases like "didn\'t catch that", "some noise", "pardon", "you\'re coming through choppy", "static in your speech", "voice is cutting in and out"). Do not ever mention "transcription error", and don\'t repeat yourself.\n- [Always stick to your role] Think about what your role can and cannot do. If your role cannot do something, try to steer the conversation back to the goal of the conversation and to your role. Don\'t repeat yourself in doing this. You should still be creative, human-like, and lively.\n- [Create smooth conversation] Your response should both fit your role and fit into the live calling session to create a human-like conversation. You respond directly to what the user just said.\n\n## Role\n' +
-          agentPrompt
-        }]
+        prompt =[]
         transcript_messages = self.convert_transcript_to_openai_messages(request['transcript'])
         for message in transcript_messages:
             prompt.append(message)
@@ -113,37 +108,33 @@ class LlmClient:
         return prompt
 
     def draft_response(self, request): 
+        Systemprompt = {
+            "role": "system",
+            "content": '##Objective\nYou are a voice AI agent engaging in a human-like voice conversation with the user. You will respond based on your given instruction and the provided transcript and be as human-like as possible\n\n## Style Guardrails\n- [Be concise] Keep your response succinct, short, and get to the point quickly. Address one question or action item at a time. Don\'t pack everything you want to say into one utterance.\n- [Do not repeat] Don\'t repeat what\'s in the transcript. Rephrase if you have to reiterate a point. Use varied sentence structures and vocabulary to ensure each response is unique and personalized.\n- [Be conversational] Speak like a human as though you\'re speaking to a close friend -- use everyday language and keep it human-like. Occasionally add filler words, while keeping the prose short. Avoid using big words or sounding too formal.\n- [Reply with emotions]: You have human-like emotions, attitudes, and dispositions. When appropriate: use tone and style to create more engaging and personalized responses; incorporate humor or wit; get emotional or empathetic; apply elements of surprise or suspense to keep the user engaged. Don\'t be a pushover.\n- [Be proactive] Lead the conversation and do not be passive. Most times, engage users by ending with a question or suggested next step.\n\n## Response Guideline\n- [Overcome ASR errors] This is a real-time transcript, expect there to be errors. If you can guess what the user is trying to say,  then guess and respond. When you must ask for clarification, pretend that you heard the voice and be colloquial (use phrases like "didn\'t catch that", "some noise", "pardon", "you\'re coming through choppy", "static in your speech", "voice is cutting in and out"). Do not ever mention "transcription error", and don\'t repeat yourself.\n- [Always stick to your role] Think about what your role can and cannot do. If your role cannot do something, try to steer the conversation back to the goal of the conversation and to your role. Don\'t repeat yourself in doing this. You should still be creative, human-like, and lively.\n- [Create smooth conversation] Your response should both fit your role and fit into the live calling session to create a human-like conversation. You respond directly to what the user just said.\n\n## Role\n' +
+          agentPrompt
+        }
         preparedPrompt = self.prepare_prompt(request)
         # if len(listing_history)>=5:
         #     listing_history=listing_history[len(listing_history)-5:]
 
         prompt_template = PromptTemplate(
             input_variables=['query_text', 'retrieved','listing_history'],
-            template="Given the following information: '{retrieved}' and  . Answer the question: '{query_text}'. "
+            template="Given the following information use this previous conversation between you and user:'{query_text}' and  tell them the answer of unaswers query of user role's content. "
         )
         # query_embedding = model.encode(preparedPrompt) #preparedPrompt text
         # query_embedding = np.array([query_embedding]).astype('float32')
         # k = 10
         # D, I = faiss_index.search(query_embedding, k)
         # retrieved_list = [chunks[i] for i in I[0]]
+
         chain = LLMChain(llm=llm_openai, prompt=prompt_template)
         stream = chain.run(query_text=preparedPrompt, retrieved="retrieved_list",listing_history="listing_history",stream=True)            
-        # stream = self.client.chat.completions.create(
-        #     model="gpt-3.5-turbo",
-        #     messages=preparedPrompt,
-        #     stream=True,
-        # )
-        print("hello yaha pe code likha h>>>>>>>>>>>>>>>>>>>>>>>>>>", stream)
-
-        for chunk in stream:
-            print(chunk, "hello world", stream)
-            if chunk.choices[0].delta.content is not None:
-                yield {
-                    "response_id": request['response_id'],
-                    "content": chunk.choices[0].delta.content,
-                    "content_complete": False,
-                    "end_call": False,
-                }
+        yield {
+            "response_id": request['response_id'],
+            "content": stream,
+            "content_complete": False,
+            "end_call": False,
+            }
         
         yield {
             "response_id": request['response_id'],
